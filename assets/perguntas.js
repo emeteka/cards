404723a8c908f62e5ce20b4d19d2af0b3f9981ab
emeteka <00001088339992sp@al.educacao.sp.gov.br>
2024-11-25 15:42:37 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Programação',
+    'HTML é linguagem?',
+    'Não'
+)
+
+criaCartao(
+    'E-sportes',
+    'Qual time brasileiro de valorant ganhou a champions?',
+    'Foi a Loud no ano de 2022'
+)
+
+criaCartao(
+    'geografia',
+    'Qual é a capital da Australia?',
+    'Camberra'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é a estrela mais quente do universo?',
+    'a estrela Rl36al'
+)
+
+criaCartao(
+    'Historia',
+    'Quantos anos durou a guerra dos 100 anos?',
+    ' durou 116 anos',
+)
+criaCartao(
+    'Historia',
+    'Quais paises fazia parte dos aliados, na Segunda Guerra Mundial?',
+    'Inglaterra, França e Estados Unidos',
+)
